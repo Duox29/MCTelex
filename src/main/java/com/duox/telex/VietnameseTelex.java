c2cc7f0a06bf8723da7c2a26f483251f27eee34e
Duox29 <leduox29@gmail.com>
2026-08-24 10:48:21 +0700

    Port to Fabric 1.21.1 (loom, JSON config, mixin for charTyped)

--- src/main/java/com/duox/telex/VietnameseTelex.java
+++ src/main/java/com/duox/telex/VietnameseTelex.java
@@ -4,20 +4,14 @@ import com.mojang.logging.LogUtils;
 
 import org.slf4j.Logger;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
-
 /**
- * Common entry point. All the interesting work happens on the client -
+ * Shared constants. All the interesting work happens on the client -
  * see {@link VietnameseTelexClient}, {@link ClientHooks} and {@link TelexEngine}.
  */
-@Mod(VietnameseTelex.MODID)
-public class VietnameseTelex {
+public final class VietnameseTelex {
     public static final String MODID = "vietnamesetelex";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public VietnameseTelex(IEventBus modEventBus, ModContainer modContainer) {
-        LOGGER.info("[VietnameseTelex] common init");
+    private VietnameseTelex() {
     }
 }
